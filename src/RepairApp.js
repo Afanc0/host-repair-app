1b9babe0f8de078254915f5e142a379316ac35ee
Afanc0 <96590805+Afanc0@users.noreply.github.com>
2023-05-28 14:29:34 -0400

    Removed String

--- src/RepairApp.js
+++ src/RepairApp.js
@@ -7,8 +7,7 @@ function RepairPage() {
     return (
         <div class="main-component">
             <img src={constr} className="home-logo" alt="logo" />
-            <strong><p>Hang Tight! Page is Under Construction. <br /> 
-            &emsp;&emsp;&emsp;&ensp; Paola, Ice Cream at 6PM?! </p></strong>
+            <strong><p>Hang Tight! Page is Under Construction.</p></strong>
         </div>
     );
 
